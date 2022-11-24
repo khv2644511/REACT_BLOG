85870e72f13bd5affc6095ce1329f0114d2ba9f6
khv2644511 <khv2644511@naver.com>
2022-11-24 20:59:19 +0900

    FEAT: 홈페이지 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import Homepage from "./pages/Homepage/Homepage";
 import TopBar from "./components/TopBar/TopBar";
+import Write from "./pages/Write/Write";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const currentUser = true;
   return (
-    <div>
+    <Router>
+      {/* 신 버전으로 하시는 분들은 Router -> BrowserRouter */}
       <TopBar />
-    </div>
+      {/* 신 버전으로 하시는 분들은 Switch -> Routes */}
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/posts">
+          <Homepage />
+        </Route>
+        <Route path="/register">
+          <h1>hello world 3</h1>
+        </Route>
+        <Route path="/login">
+          <h1>hello world 4</h1>
+        </Route>
+        <Route path="/post/:id">
+          <h1>hello world 5</h1>
+        </Route>
+        <Route path="/write">
+          <Write />
+          <h1>hello world 6</h1>
+        </Route>
+        <Route path="/settings">
+          <h1>hello world 7</h1>
+        </Route>
+        <Route path="/about">
+          <About />
+          <h1>hello world 7</h1>
+        </Route>
+        <Route path="/contact">
+          <Contact />
+          <h1>hello world 7</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./topbar.css";
 
 export default function TopBar() {
+  const user = true;
   return (
     <div className="top">
       <div className="topLeft">
@@ -10,16 +12,64 @@ export default function TopBar() {
         <i className="fab fa-pinterest-square"></i>
         <i className="fab fa-twitter-square"></i>
       </div>
+
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/about">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/contact">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && (
+            <li className="topListItem">
+              <Link className="link" to="/logout">
+                LOGOUT
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
-      <div className="topRight"></div>
+
+      <div className="topRight">
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
     </div>
   );
 }
