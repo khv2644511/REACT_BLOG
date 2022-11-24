31b676e2d0eee48c0388dad9acabd46cacfe14ec
khv2644511 <khv2644511@naver.com>
2022-11-24 19:48:37 +0900

    FEAT: TopBar

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./topbar.css";
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="fab fa-facebook-square"></i>
+        <i className="fab fa-instagram-square"></i>
+        <i className="fab fa-pinterest-square"></i>
+        <i className="fab fa-twitter-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight"></div>
+    </div>
+  );
+}
